bc9bb31be61b70d7e75c82368f41c9ecea748913
Thomas Aspbury <tomaspbury@icloud.com>
2024-01-29 16:18:59 +0000

    refactor: inject host value from envs during google auth flow

--- client/src/components/GoogleSignIn/GoogleSignIn.jsx
+++ client/src/components/GoogleSignIn/GoogleSignIn.jsx
@@ -1,10 +1,10 @@
-import { useGoogleLogin } from '@react-oauth/google';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { useGoogleLogin } from "@react-oauth/google";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
-import { useAuth } from '../../contexts/AuthContext';
+import { useAuth } from "../../contexts/AuthContext";
 
-import './GoogleSignIn.css';
+import "./GoogleSignIn.css";
 
 const GoogleSignIn = () => {
   const { logIn } = useAuth();
@@ -12,25 +12,28 @@ const GoogleSignIn = () => {
   const continueWithGoogle = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       try {
-        const res = await fetch('http://localhost:3000/api/auth/google', {
-          method: 'POST',
-          mode: 'cors',
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            access_token: tokenResponse.access_token,
-          }),
-        });
-  
+        const res = await fetch(
+          `http://${process.env.HOST}:3000/api/auth/google`,
+          {
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              access_token: tokenResponse.access_token,
+            }),
+          }
+        );
+
         const body = await res.json();
-  
+
         // User record may have been created (200) or just retrieved (201)
         if (res.status === 200 || res.status === 201) {
           return logIn(body);
         }
-  
+
         if (res.status == 500) {
           throw new Error(body.message);
         }
@@ -40,16 +43,12 @@ const GoogleSignIn = () => {
       }
     },
     onError: (errorResponse) => console.log(errorResponse),
-    flow: 'implicit',
-    scope: 'profile email',
+    flow: "implicit",
+    scope: "profile email",
   });
 
   return (
-    <button
-      className="googleSignIn"
-      type="button"
-      onClick={continueWithGoogle}
-    >
+    <button className="googleSignIn" type="button" onClick={continueWithGoogle}>
       <FontAwesomeIcon className="googleSignIn__icon" icon={faGoogle} />
       GOOGLE SIGN IN
     </button>
